4754ec55acf8a91a1b72cbc0b84180b073a92da9
ayselmehmetzade <ayselmehmetzade@hotmail.com>
2021-06-13 18:44:55 +0300

    jobpostinglistelendi

--- src/pages/jobPosting/jobPostingList.js
+++ src/pages/jobPosting/jobPostingList.js
@@ -1,41 +1,45 @@
-import React from "react";
-import { Icon, Label, Menu, Table } from "semantic-ui-react";
+import React, { useState, useEffect } from "react";
+import { Icon, Menu, Table } from "semantic-ui-react";
+import JobPostingService from "../../services/jobPostingService";
 
 export default function JobPostList() {
+  const [jobPostings, setJobPostings] = useState([]);
+
+  useEffect(() => {
+    let jobPostingService = new JobPostingService();
+    jobPostingService
+      .getJobPostings()
+      .then((result) => setJobPostings(result.data.data));
+  }, []);
+
   return (
     <div>
       <Table celled>
         <Table.Header>
           <Table.Row>
-            <Table.HeaderCell>Header</Table.HeaderCell>
-            <Table.HeaderCell>Header</Table.HeaderCell>
-            <Table.HeaderCell>Header</Table.HeaderCell>
+            <Table.HeaderCell>Job Title</Table.HeaderCell>
+            <Table.HeaderCell>Company Name</Table.HeaderCell>
+            <Table.HeaderCell>City</Table.HeaderCell>
+            <Table.HeaderCell>Open Position Quota</Table.HeaderCell>
+            <Table.HeaderCell>Application Deadline</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
 
         <Table.Body>
-          <Table.Row>
-            <Table.Cell>
-              <Label ribbon>First</Label>
-            </Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>Cell</Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>Cell</Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-            <Table.Cell>Cell</Table.Cell>
-          </Table.Row>
+          {jobPostings.map((jobPosting) => (
+            <Table.Row key={jobPosting.id}>
+              <Table.Cell>{jobPosting.jobPosition.jobTitle}</Table.Cell>
+              <Table.Cell>{jobPosting.employer.companyName}</Table.Cell>
+              <Table.Cell>{jobPosting.city.name}</Table.Cell>
+              <Table.Cell>{jobPosting.openPositionQuota}</Table.Cell>
+              <Table.Cell>{jobPosting.applicationDeadline}</Table.Cell>
+            </Table.Row>
+          ))}
         </Table.Body>
 
         <Table.Footer>
           <Table.Row>
-            <Table.HeaderCell colSpan="3">
+            <Table.HeaderCell colSpan="5">
               <Menu floated="right" pagination>
                 <Menu.Item as="a" icon>
                   <Icon name="chevron left" />
