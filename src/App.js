b7af0ba101cb8c944ed26e7edaa93b369bc0dc6c
ayselmehmetzade <ayselmehmetzade@hotmail.com>
2021-06-13 18:15:51 +0300

    jobpostings added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Dashboard from "./layouts/Dashboard";
 import Navi from "./layouts/Navi";
+import JobPostList from "./pages/jobPosting/jobPostingList";
+import "semantic-ui-css/semantic.min.css";
+import { Container } from "semantic-ui-react";
 
 function App() {
   return (
     <div className="App">
-      <Navi></Navi>
-      <Dashboard></Dashboard>
+      <Container className="main">
+        <Navi></Navi>
+        <Dashboard></Dashboard>
+        <JobPostList></JobPostList>
+      </Container>
     </div>
   );
 }
--- src/layouts/Navi.js
+++ src/layouts/Navi.js
@@ -1,5 +1,30 @@
 import React from "react";
+import "../styles/Navi.scss";
+import { Button, Container, Dropdown, Menu } from "semantic-ui-react";
 
 export default function Navi() {
-  return <div>Navi</div>;
+  return (
+    <div>
+      <Menu size="tiny" inverted fixed="top">
+        <Container>
+          <Menu.Item name="home" />
+          <Menu.Item name="messages" />
+
+          <Menu.Menu position="right">
+            <Dropdown item text="Language">
+              <Dropdown.Menu>
+                <Dropdown.Item>English</Dropdown.Item>
+                <Dropdown.Item>Russian</Dropdown.Item>
+                <Dropdown.Item>Spanish</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+
+            <Menu.Item>
+              <Button primary>Sign Up</Button>
+            </Menu.Item>
+          </Menu.Menu>
+        </Container>
+      </Menu>
+    </div>
+  );
 }
